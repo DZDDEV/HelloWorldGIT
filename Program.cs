934a05735a26e16b8beb1568b52883d84f261154
sayah imad <sayahimad@outlook.fr>
2015-06-17 22:42:54 +0100

    GIT
    HELLO CODE

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///FIRST CODE ON GIT
+            Console.WriteLine("HELLO WORLD GIT");
+            Console.ReadKey();
+        }
+    }
+}
